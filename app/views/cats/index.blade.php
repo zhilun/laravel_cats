a67202d6aa712e5c9c7f4b4f6137592de5651463
zhilun <trankynam2612@gmail.com>
2014-11-26 11:09:42 +0700

    edit format

--- app/views/cats/index.blade.php
+++ app/views/cats/index.blade.php
@@ -1,21 +1,23 @@
 @extends('master')
+
 @section('header')
-@if(isset($breed))
-{{link_to('/', 'Back to the overview')}}
-@endif
-<h2>
-All @if(isset($breed)) {{$breed->name}} @endif Cats
-<a href="{{url('cats/create')}}" class="btn btn-primary pull-right">
-Add a new cat
-</a>
-</h2>
+	@if(isset($breed))
+		{{link_to('/', 'Back to the overview')}}
+	@endif
+	<h2>
+		All @if(isset($breed)) {{$breed->name}} @endif Cats
+		<a href="{{url('cats/create')}}" class="btn btn-primary pull-right">
+			Add a new cat
+		</a>
+	</h2>
 @stop
+
 @section('content')
-@foreach($cats as $cat)
-<div class="cat">
-    <a href="{{url('cats/'.$cat->id)}}">
-    <strong> {{{$cat->name}}} </strong> - {{{$cat->breed->name}}}
-    </a>
-</div>
-@endforeach
+	@foreach($cats as $cat)
+		<div class="cat">
+		    <a href="{{url('cats/'.$cat->id)}}">
+		    	<strong> {{{$cat->name}}} </strong> - {{{$cat->breed->name}}}
+		    </a>
+		</div>
+	@endforeach
 @stop
